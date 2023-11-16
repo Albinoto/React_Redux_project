0ac6722c651a8bff645e4745ca5a9ec5ce9d973b
Alberto Notonica <notoalbi@gmail.com>
2023-11-16 16:36:36 +0100

    update

--- src/screen/home-screen.js
+++ src/screen/home-screen.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Layout from "../components/layout";
+import Box from "../components/styled"
 
 const HomePage = () => {
   return (
     <Layout>
       <h1>Home</h1>
+      <Box 
+        width="400px" 
+        height="239px" 
+        border="1px solid" 
+        borderColor="red" 
+        borderRadius="16px" 
+        my="50px"
+        bg="green"></Box>
     </Layout>
   );
 };
--- src/screen/paginated-screen.js
+++ src/screen/paginated-screen.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Layout from "../components/layout";
+import { useParams } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux";
 import PhotoSection from "../components/Photo-Section";
 import Paginator from "../components/Paginator";
@@ -8,9 +9,11 @@ import Paginator from "../components/Paginator";
 import { useParams } from "react-router-dom";
 
 const PaginatedScreen = () => {
+  const { page } = useParams();
+
   return (
     <Layout>
-      <h1>Paginated</h1>
+      <h1>Paginated: {page}</h1>
     </Layout>
   );
 };
